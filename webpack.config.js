110348ab27e27df61af5d34c74ae3c57b9a0617d
admin <admin@eq19.com>
2022-08-18 23:30:07 +0700

    script.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    theme: ["./js/theme.js", "./_sass/theme.scss"],
+    theme: ["./js/script.js", "./_sass/theme.scss"],
   },
   output: {
     filename: "js/[name].min.js",
