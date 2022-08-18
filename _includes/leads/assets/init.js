9e1b854c93ce7d74538d4f56b06ee28ff796b591
admin <admin@eq19.com>
2022-08-19 02:31:01 +0700

    Update init.js

--- _includes/leads/assets/init.js
+++ _includes/leads/assets/init.js
@@ -17,8 +17,59 @@
  */
 jQuery(function($)
 {
+	// jQuery document.ready will be executed just after html dom tree has been parsed out.
+	// So it is far more earlier executed than window onload.
 	$(document).ready(function()
 	{
+		// to stick navbar on top and hash
+		top_menu_height = $('.top-menu').height();
+		$('html,body').scrollspy({target: '#templatemo-nav-bar', offset: top_menu_height + 10});
+
+		// do scroll and clear the hash anytime someone arrives with a hash tag
+		// https://stackoverflow.com/a/50688363/4058484
+		if( typeof(location.hash) !== 'undefined' && location.hash.length ) 
+		{
+			var location_hash = location.hash.split('?')[0];
+			history.replaceState(null, null, location.pathname);
+			scrollTo(location_hash);
+		}
+
+		// set links which point outside
+		$('.external-link').unbind('click');
+		$(document.links).filter(function() {
+			return this.hostname != window.location.hostname;
+		}).attr('target', '_blank'); 
+
+		// scroll to top
+		$('#btn-back-to-top').click(function(e)
+		{
+			e.preventDefault();
+			scrollTo('#templatemo-top');
+		});
+
+		// scroll to specific id when click on link
+		$('.internal-link, .carousel-inner a').click(function(e)
+		{
+			e.preventDefault(); 
+			var linkId = $(this).attr('href');
+			scrollTo(linkId);
+			return false;
+		});
+
+		// scroll to specific id when click on menu
+		$('.top-menu .navbar-nav a').click(function(e)
+		{
+			e.preventDefault(); 
+			var linkId = $(this).attr('href');
+			scrollTo(linkId);
+			if($('.navbar-toggle').is(":visible") == true)
+			{
+				$('.navbar-collapse').collapse('toggle');
+			}
+			$(this).blur();
+			return false;
+		});
+
 		// https://stackoverflow.com/q/15674733/4058484
 		$('#₠Quantum').empty();
 		if (!$('#diagram').length) {
@@ -31,7 +82,8 @@ jQuery(function($)
 				{%- endfor -%}
 			{%- endcapture %}
 			$('#₠Quantum').append('{{ skema | strip_newlines | split: " " | join: " " }}');
-		};  
+		};
+		window.include(url) = function(){$('#₠Quantum').append('<script src="{{ site.url }}/' + url + '"></script>');return false;}
 		{%- capture skema -%}{%- include extra/rest/script.liquid -%}{%- endcapture %}
 		$('#₠Quantum').append('{{ skema | strip_newlines | split: " " | join: " " }}');
 
