e13d5cf0d09d34a3b76f10c93f865673042a62f6
admin <admin@eq19.com>
2022-08-19 03:34:21 +0700

    Update init.js

--- _includes/leads/assets/init.js
+++ _includes/leads/assets/init.js
@@ -33,8 +33,8 @@ jQuery(function($)
 			$('#₠Quantum').append('{{ skema | strip_newlines | split: " " | join: " " }}');
 		};  
 		{%- capture skema -%}{%- include extra/rest/script.liquid -%}{%- endcapture %}
-		window.tag = function(url){return '<script src="{{ site.url }}/' + url + '"></script>';}
-		window.include = function(url){$('#₠Quantum').append(tag(url)); return false;}
+		var tag = function(url){return '<script src="{{ site.url }}/' + url + '"></script>';}
+		var include = function(url){$('#₠Quantum').append(tag(url)); return false;}
 		$('#₠Quantum').append('{{ skema | strip_newlines | split: " " | join: " " }}');
 
 		// https://api.jqueryui.com/uniqueId/
