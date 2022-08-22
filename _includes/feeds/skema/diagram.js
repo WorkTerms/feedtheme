c8b2cdee70b63b2e4738af789d2723399ad2890b
admin <admin@eq19.com>
2022-08-22 10:25:03 +0700

    Update diagram.js

--- _includes/feeds/skema/diagram.js
+++ _includes/feeds/skema/diagram.js
@@ -160,7 +160,7 @@ var id, js, ids, pad, xml, back, data, feed, json, link, init, size, test, type,
 			//https://stackoverflow.com/a/18266882/4058484
 			if (elements) {
 				elements.each(function(index) {draw.node(index, this);});
-				$('html').find('*').each(function() {$(this).uniqueId();});
+				$('html,body').find('*').each(function() {$(this).uniqueId();});
 				if (type != 'Tree') {elements.on('click', function(){draw.click(this);});};
 				if (type != 'Route') {elements.on('dblclick', function(){draw.dblclick(this);});};
 			}
