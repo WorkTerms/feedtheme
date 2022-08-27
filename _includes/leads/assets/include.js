1e21dc995986aeb188fb502a10ea7eb7aee83f2a
admin <admin@eq19.com>
2022-08-27 07:24:31 +0700

    include('js/legacy.js');

--- _includes/leads/assets/include.js
+++ _includes/leads/assets/include.js
@@ -1,11 +1,9 @@
 /* core.JS
  ========================================================*/
-include('slick/slick/slick.min.js');
-include('sequence/js/svginnerhtml.min.js');
-include('sequence/js/snap.svg-min.js');
-include('flowchart/raphael-min.js');
-include('route/d3.min.js');
-include('cards/widget.js');
+include('js/legacy.js');
+include('js/camera.js');
+include('js/isotope.min.js');
+include('js/owl.carousel.js');
 
 /* library
  ========================================================*/
@@ -26,12 +24,15 @@ include('js/jquery.simplr.smoothscroll.min.js');*/
  ========================================================*/
 include('js/superfish.js');
 include('js/jquery.mobilemenu.js');
+include('js/jquery.mobile.customized.min.js');
 
 /* Custom script
  ========================================================*/
-include('js/legacy.js');
-include('js/camera.js');
-include('js/isotope.min.js');
-include('js/owl.carousel.js');
+include('slick/slick/slick.min.js');
+include('sequence/js/svginnerhtml.min.js');
+include('sequence/js/snap.svg-min.js');
+include('flowchart/raphael-min.js');
+include('route/d3.min.js');
+include('cards/widget.js');
 include('js/function.js');
 
