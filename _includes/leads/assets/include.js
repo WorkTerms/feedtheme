6cbdc1510ef5f759a17dd3e6fe7ba954ca657682
eq19 <admin@eq19.com>
2022-09-04 14:47:02 +0700

    includeHTML

--- _includes/leads/assets/include.js
+++ _includes/leads/assets/include.js
@@ -1,36 +1,36 @@
 /* core.JS
  ========================================================*/
-include('js/modal.js');
-include('js/legacy.js');
-include('js/TMForm.js');
+includeHTML('js/modal.js');
+includeHTML('js/legacy.js');
+includeHTML('js/TMForm.js');
 
 /* library
  ========================================================*/
-include('js/device.min.js');
-include('js/jquery.cookie.js');
-include('js/jquery.ui.totop.js');
-include('js/jquery.easing.1.3.js');
-include('js/jquery.equalheights.js');
-include('js/jquery-migrate-1.2.1.js');
+includeHTML('js/device.min.js');
+includeHTML('js/jquery.cookie.js');
+includeHTML('js/jquery.ui.totop.js');
+includeHTML('js/jquery.easing.1.3.js');
+includeHTML('js/jquery.equalheights.js');
+includeHTML('js/jquery-migrate-1.2.1.js');
 
 
 /* Smooth scrolling
  ========================================================
-include('wheel/jquery.mousewheel.min.js');
-include('js/jquery.simplr.smoothscroll.min.js');*/
+includeHTML('wheel/jquery.mousewheel.min.js');
+includeHTML('js/jquery.simplr.smoothscroll.min.js');*/
 
 /* Superfish menu
  ========================================================*/
-include('js/superfish.js');
-include('js/jquery.mobilemenu.js');
-include('js/jquery.mobile.customized.min.js');
+includeHTML('js/superfish.js');
+includeHTML('js/jquery.mobilemenu.js');
+includeHTML('js/jquery.mobile.customized.min.js');
 
 /* Custom script
  ========================================================*/
-include('slick/slick/slick.min.js');
-include('sequence/js/svginnerhtml.min.js');
-include('sequence/js/snap.svg-min.js');
-include('flowchart/raphael-min.js');
-include('route/d3.min.js');
-include('cards/widget.js');
-include('js/function.js');
+includeHTML('slick/slick/slick.min.js');
+includeHTML('sequence/js/svginnerhtml.min.js');
+includeHTML('sequence/js/snap.svg-min.js');
+includeHTML('flowchart/raphael-min.js');
+includeHTML('route/d3.min.js');
+includeHTML('cards/widget.js');
+includeHTML('js/function.js');
